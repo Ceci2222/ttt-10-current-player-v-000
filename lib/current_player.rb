db071c8713b884f5e65dc6923531634a8598b328
Marie Burns <mcburns2222@gmail.com>
2018-10-29 04:22:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,8 @@ def turn_count(board)
   board = [" "," "," "," "," "," "," "," "," "]
   counter = 0
   board.each do |board|
-    counter += 1
+    if board == "X" || board == "O"
+      counter += 1
     end
 end
 
